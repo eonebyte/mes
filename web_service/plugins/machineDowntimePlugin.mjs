35999ebfa434a408602b74d08fe4fe3dca597913
Eone Byte <eonebyte@gmail.com>
2024-11-13 21:01:24 +0700

    update

--- web_service/plugins/machineDowntimePlugin.mjs
+++
@@ -1,9 +0,0 @@
-import CreateDowntimeLog from "../modules/machine_downtime/injectEvent.mjs"
-import { GetMachines, ReadDowntimeLog } from "../modules/machine_downtime/machineEvents.mjs";
-
-
-export default async function machineDowntimePlugin(fastify, opts) {
-   await fastify.register(CreateDowntimeLog);
-   await fastify.register(GetMachines);
-   await fastify.register(ReadDowntimeLog);
-}
--- web_service/plugins/webSocketPlugin.mjs
+++
@@ -1,6 +0,0 @@
-import DowntimeSocket from "../modules/web_socket/downtimeSocket.mjs";
-
-export default async function webSocketPlugin(fastify, opts) {
-  fastify.register(DowntimeSocket);
-}
-
